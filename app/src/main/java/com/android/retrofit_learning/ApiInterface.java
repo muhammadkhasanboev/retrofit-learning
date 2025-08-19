0197ae6c7e49465667b22290caf8274168c6f5a9
Muhammad <muhammadxasanboyev1234@gmail.com>
2025-08-19 15:07:52 +0500

    api interface created

--- app/src/main/java/com/android/retrofit_learning/ApiInterface.java
+++ app/src/main/java/com/android/retrofit_learning/ApiInterface.java
@@ -1,9 +1,16 @@
 package com.android.retrofit_learning;
 
+import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 interface ApiInterface{
-//    @GET("api.php")
-
+    @GET("api.php")
+    Call<DataClass> getResponse(
+            @Query("amount") int amount,
+            @Query("category") String category,
+            @Query("difficulty") String difficulty,
+            @Query("type") String type
+    );
 
 }
