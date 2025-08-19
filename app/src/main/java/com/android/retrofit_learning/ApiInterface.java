87b93965fbdd157fe75769551c370f227710bcf7
Muhammad <muhammadxasanboyev1234@gmail.com>
2025-08-19 15:02:25 +0500

    data class created

---
+++ app/src/main/java/com/android/retrofit_learning/ApiInterface.java
@@ -0,0 +1,9 @@
+package com.android.retrofit_learning;
+
+import retrofit2.http.GET;
+
+interface ApiInterface{
+//    @GET("api.php")
+
+
+}
---
+++ app/src/main/java/com/android/retrofit_learning/DataClass.java
@@ -0,0 +1,58 @@
+package com.android.retrofit_learning;
+import java.util.List;
+
+public class DataClass{
+    List<Results> results;
+
+    public List<Results> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Results> results) {
+        this.results = results;
+    }
+
+    class Results{
+        String type, difficulty, category, question, correct_answer;
+
+        public String getDifficulty() {
+            return difficulty;
+        }
+
+        public void setDifficulty(String difficulty) {
+            this.difficulty = difficulty;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getCategory() {
+            return category;
+        }
+
+        public void setCategory(String category) {
+            this.category = category;
+        }
+
+        public String getQuestion() {
+            return question;
+        }
+
+        public void setQuestion(String question) {
+            this.question = question;
+        }
+
+        public String getCorrect_answer() {
+            return correct_answer;
+        }
+
+        public void setCorrect_answer(String correct_answer) {
+            this.correct_answer = correct_answer;
+        }
+    }
+}
